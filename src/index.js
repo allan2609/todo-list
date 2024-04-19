c02998e746f402392940db2df07e7d16a5fbed13
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-04-19 11:10:33 +0300

    Add edit and remove icons for todos

--- src/index.js
+++ src/index.js
@@ -79,12 +79,6 @@ function createProjectSelector() {
     showTodos();
   });
   
-  /*projects.forEach(project => { 
-    const option = document.createElement("option");
-    option.textContent = project.projectName;
-    selector.appendChild(option);
-  });*/
-  
   projectSelector.appendChild(selector);
 }
 
@@ -94,9 +88,31 @@ function showTodos() {
     todoList.removeChild(todoList.firstChild);
   }
   projects[activeProject].todos.forEach(todo => { 
-    const item = document.createElement("p");
+    const item = document.createElement("span");
     item.className = "todo-item";
-    item.textContent = todo.title;
+    
+    const title = document.createElement("div");
+    title.className = "title";
+    title.textContent = todo.title;
+    
+    const date = document.createElement("div");
+    date.className = "date";
+    date.textContent = todo.dueDate;
+    
+    const edit = document.createElement("div");
+    const editIcon = '<svg viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
+    edit.className = "edit";
+    edit.innerHTML = editIcon;
+    
+    const remove = document.createElement("div");
+    const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
+    remove.className = "remove";
+    remove.innerHTML = removeIcon;
+    
+    item.appendChild(title);
+    item.appendChild(date);
+    item.appendChild(edit);
+    item.appendChild(remove);
     todoList.appendChild(item);
   });
 }
