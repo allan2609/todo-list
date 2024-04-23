5f956ca723efb2a2671a6cc34c40dae3e4b99a49
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-04-23 19:32:03 +0300

    Create functions to remove todos and projects

--- src/index.js
+++ src/index.js
@@ -23,9 +23,10 @@ function addToProject() {
   title.value = "";
   description.value = "";
   date.value = "";
-  priority.value = "";
+  priority.value = "medium";
   
   showTodos();
+  todoDialog.close();
 }
 
 function createNewProject() {
@@ -35,7 +36,11 @@ function createNewProject() {
   );
   console.log(projects);
   showProjects();
+  projectDialog.close();
   projectName.value = "";
+  if (projects.length != 0) {
+    document.querySelector(".new-todo").disabled = false;
+  }
 }
 
 function showProjects() {
@@ -78,14 +83,28 @@ function createProjectSelector() {
   projectSelector.appendChild(selector);
 }
 
-document.querySelector(".add-todo").addEventListener("click", addToProject);
+document.querySelector(".add-todo").addEventListener("click", () => {
+  if (validateTodoForm()) {
+    addToProject();
+  }
+});
+  
 document.querySelector(".close-todo-dialog").addEventListener("click", () => {
   todoDialog.close();
 });
 
-document.querySelector(".add-project").addEventListener("click", createNewProject);
+document.querySelector(".add-project").addEventListener("click", () => {
+  if (validateProjectForm()) {
+    createNewProject();
+  }
+});
+  
 document.querySelector(".close-project-dialog").addEventListener("click", () => {
   projectDialog.close();
+});
+
+document.querySelector(".close-project-remove-dialog").addEventListener("click", () => {
+  removeProjectDialog.close();
 });
 
 function showTodos() {
@@ -93,38 +112,43 @@ function showTodos() {
   while (todoList.hasChildNodes()) {
     todoList.removeChild(todoList.firstChild);
   }
-  projects[activeProject].todos.forEach(todo => { 
-    const item = document.createElement("span");
-    item.className = "todo-item";
-    
-    const title = document.createElement("div");
-    title.className = "title";
-    title.textContent = todo.title;
-    
-    const date = document.createElement("div");
-    date.className = "date";
-    date.textContent = todo.dueDate;
-    
-    const edit = document.createElement("div");
-    const editIcon = '<svg viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
-    edit.className = "edit";
-    edit.innerHTML = editIcon;
-    
-    const remove = document.createElement("div");
-    const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
-    remove.className = "remove";
-    remove.innerHTML = removeIcon;
-    
-    item.appendChild(title);
-    item.appendChild(date);
-    item.appendChild(edit);
-    item.appendChild(remove);
-    todoList.appendChild(item);
-  });
+  if (projects.length > 0) {
+    for (let i = 0; i < projects[activeProject].todos.length; i++) {
+      const item = document.createElement("span");
+      item.className = "todo-item";
+      item.dataset.itemID = projects[activeProject].todos[i].id;
+
+      const title = document.createElement("div");
+      title.className = "title";
+      title.textContent = projects[activeProject].todos[i].title;
+
+      const date = document.createElement("div");
+      date.className = "date";
+      date.textContent = projects[activeProject].todos[i].dueDate;
+
+      const edit = document.createElement("div");
+      const editIcon = '<svg viewBox="0 0 24 24"><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
+      edit.className = "edit";
+      edit.innerHTML = editIcon;
+
+      const remove = document.createElement("div");
+      const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
+      remove.className = "remove";
+      remove.innerHTML = removeIcon;
+      remove.addEventListener("click", e => removeTodo(e));
+
+      item.appendChild(title);
+      item.appendChild(date);
+      item.appendChild(edit);
+      item.appendChild(remove);
+      todoList.appendChild(item);
+    };
+  }
 }
 
 const todoDialog = document.querySelector("#todo-dialog");
 const projectDialog = document.querySelector("#project-dialog");
+const removeProjectDialog = document.querySelector("#remove-project-dialog");
 
 document.querySelector(".new-todo").addEventListener("click", () => {
   todoDialog.showModal();
@@ -133,3 +157,62 @@ document.querySelector(".new-todo").addEventListener("click", () => {
 document.querySelector(".new-project").addEventListener("click", () => {
   projectDialog.showModal();
 });
+
+document.querySelector(".remove-project").addEventListener("click", () => {
+  removeProjectDialog.showModal();
+});
+
+function removeTodo(event) {
+  const targetTodo = event.target.closest(".todo-item").dataset.itemID;
+  const index = projects[activeProject].todos.findIndex(todo => todo.id == targetTodo);
+  projects[activeProject].todos.splice(index, 1);
+  showTodos();
+};
+
+function validateTodoForm() {
+  const title = document.querySelector("#title");
+  const description = document.querySelector("#description");
+  const date = document.querySelector("#due-date");
+  
+  if (title.value.length < 3) {
+    alert("Title must be filled out");
+    return false;
+  } else if (title.value.length > 30) {
+    alert("Title too long");
+    return false;
+  } else if (description.value.length > 120) {
+    alert("Description too long");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateProjectForm() {
+  const name = document.querySelector("#project-name");
+  if (name.value.length < 3) {
+    alert("Project name must be filled out");
+    return false;
+  } else if (name.value.length > 30) {
+    alert("Project name too long");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+document.querySelector(".remove-project-permanently").addEventListener("click", removeCurrentProject);
+
+function removeCurrentProject() {
+  projects.splice(activeProject, 1);
+  removeProjectDialog.close();
+  createProjectSelector();
+  if (activeProject > 0) {
+    activeProject -= 1;
+  }
+  console.log("removing project. active project becomes: " + activeProject);
+  showTodos();
+  if (projects.length == 0) {
+    document.querySelector(".new-todo").disabled = true;
+  }
+}
